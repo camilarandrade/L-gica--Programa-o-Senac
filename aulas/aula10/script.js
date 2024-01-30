c466405ff4bfebbc9ae4a06da0f42cbd4b12d3b4
CAMILA ROCHA DE ANDRADE <camila.randrade@senacsp.edu.br>
2024-01-29 22:02:00 -0300

    atualização atividade

--- aulas/aula10/script.js
+++ aulas/aula10/script.js
@@ -1,24 +1,16 @@
 /*
 Parte 1: Crie uma classe Personagem. O personagem deverá ter:
 nome, vida, ataque, defesa, vivo (ou morto), posicao (numero inteiro)
-lembre de criar o método construtor 
-
+lembre de criar o método construtor
+ 
 Parte 2: Crie a função/método morrer
     - propriedade "vivo" se torna false, e console.log("morreu")
 Crie a função tomarDano
     - tomar dano recebe um parametro, que é o valor do dano
     - diminui os pontos de vida do personagem, com base no dano tomado
     - verifica, se o dano for maior que os pontos de vida, personagem morre
-    - se os pontos de vida forem menor ou igual a zero, personagem morre
-    - ao morrer, personagem continua com 0 pontos de vida
-    - o personagem só pode tomar dano se estiver vivo
-
-Crie a função atacar
-    - atacar, recebe como parametro um personaem/inimigo
-    - so pode atacar alguém com vida
-    - exibe o console.log informando qual a força do ataque e inimigo atacado
 */
-
+ 
 class Personagem {
     constructor(nome, ataque, defesa, vida, posicao, vivo = true) {
         this.nome = nome;
@@ -27,18 +19,19 @@ class Personagem {
         this.posicao = posicao;
         this.vida = vida;
         this.vivo = vivo;
+        this.vidaMaxima = vida
     }
-
+ 
     morrer() {
         this.vivo = false;
         console.log(`O ${this.nome} morreu!`);
     }
-
+ 
     tomarDano(quantidade) {
         if (this.vivo) {            
             this.vida = this.vida - quantidade;
             console.log(`${this.nome} sofreu ${quantidade} de dano, e agora tem ${this.vida} de vida`)
-
+ 
             if (this.vida <= 0) {
                 this.vida = 0;
                 this.morrer();
@@ -47,7 +40,7 @@ class Personagem {
             console.log(`${this.nome} não pode mais sofrer dano!`);
         }
     }
-
+ 
     atacar(inimigo) {
         if(inimigo.vivo && this.vivo) {
             console.log(`${this.nome} atacou ${inimigo.nome} com força de ${this.ataque}`);
@@ -56,15 +49,30 @@ class Personagem {
             console.log("Não é possível atacar na condição de morto!");
         }
     }
+ 
+ 
+ 
+    pocao(quantidade){
+        if(this.vivo && this.vida <= this.vidaMaxima){
+            this.vida =+ quantidade;
+            console.log(`${this.nome} ganhou ${quantidade} de poção, e agora tem ${this.vidaMaxima} de vida`)
+       
+            if (this.vida > this.vidaMaxima) {
+                this.vida = this.vidaMaxima
+            }    
+       
+        } else {
+            console.log(`${this.nome} não pode mais se recuperar!`);
+        }
+       
+    }
 }
-
+ 
 let personagem1 = new Personagem("Arthur", 10, 12, 100, 1, true);
 let personagem2 = new Personagem("Gendalf", 12, 8, 85, 1);
-
+ 
 console.log(personagem1);
 console.log(personagem2);
-
+ 
 console.log(personagem1.atacar(personagem2));
-console.log(personagem2.atacar(personagem1));
-
-
+console.log(personagem2.pocao(30));
