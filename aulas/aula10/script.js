d19dbdf7bad35516f35e7c5a7426643ccb68969b
CAMILA ROCHA DE ANDRADE <camila.randrade@senacsp.edu.br>
2024-01-31 21:51:26 -0300

    atualizações

--- aulas/aula10/script.js
+++ aulas/aula10/script.js
@@ -22,6 +22,26 @@ Exercício:
     - Crie um limite de vida máxima para o personagem
     - Crie uma função de recuperar vida que recebe um total de pontos para recuperar
     - A recuperação de via não pode fazer o personaem ir além da vida máxima
+
+Parte 3: Crie a herança
+    - Crie as classes Arqueiro, Guerreiro e Mago
+    - Todas as 3 classes herdam de personagem
+
+Alterações do Guerreiro:
+    - Acrescente a propriedade "escudo" na classe Guerreiro.
+    - A função tomarDano do Guerreiro deve proteger seus pontos de vida,
+    abatendo o dano sofrido dos pontos do seu escudo.    
+    
+    - Acrescente a sobrescrita da função atacar, verificando a posição do inimigo
+    - Se o inimigo estiver a mais de 1 de distância, o guerreiro não pode atacar.
+
+Alterações do Arqueiro:
+    - O arqueiro só pode atacar se a distancia dele para o oponente for maior do que 3.
+    - O arqueiro tem um totalDeFlechas.
+    - O arqueiro só pode atacar se o total de flechas for maior que 0
+
+    - Crie a função recarregarFlehca, que recebe por parametro a quantidade a recarregar
+    - A função deve retornar o total de flechas recarregadas.
 */
 
 class Personagem {
@@ -37,11 +57,11 @@ class Personagem {
 
     morrer() {
         this.vivo = false;
-        console.log(`O ${this.nome} morreu!`);
+        console.log(`O ${this.nome} morreu!`);        
     }
 
     tomarDano(quantidade) {
-        if (this.vivo) {            
+        if (this.vivo) {
             this.vida = this.vida - quantidade;
             console.log(`${this.nome} sofreu ${quantidade} de dano, e agora tem ${this.vida} de vida`)
 
@@ -55,7 +75,7 @@ class Personagem {
     }
 
     atacar(inimigo) {
-        if(inimigo.vivo && this.vivo) {
+        if (inimigo.vivo && this.vivo) {
             console.log(`${this.nome} atacou ${inimigo.nome} com força de ${this.ataque}`);
             inimigo.tomarDano(this.ataque);
         } else {
@@ -63,10 +83,10 @@ class Personagem {
         }
     }
 
-    recuperarVida(vidaRecebida, personagem){
-        personagem.vida += vidaRecebida;        
+    recuperarVida(vidaRecebida, personagem = this) {
+        personagem.vida += vidaRecebida;
 
-        if(personagem.vida > personagem.vidaMaxima) {
+        if (personagem.vida > personagem.vidaMaxima) {
             personagem.vida = personagem.vidaMaxima;
         }
 
@@ -74,12 +94,71 @@ class Personagem {
     }
 }
 
-let personagem1 = new Personagem("Arthur", 10, 12, 100, 1, true);
-let personagem2 = new Personagem("Gendalf", 2, 8, 85, 1);
+class Arqueiro extends Personagem {
+    constructor(nome, ataque, defesa, vida, posicao, vivo = true, totalDeFlechas) {
+        super(nome, ataque, defesa, vida, posicao, vivo);
+        this.totalDeFlechas = totalDeFlechas;
+    }
 
-console.log(personagem1);
-console.log(personagem2);
+    atacar(inimigo) {
+        if(this.totalDeFlechas > 0 && Math.abs(this.posicao - inimigo.posicao) > 3) {
+            super.atacar(inimigo);
+            this.totalDeFlechas -= 1;
+        } else if(!(this.totalDeFlechas > 0) && Math.abs(this.posicao - inimigo.posicao) > 3) {
+            console.log(`${this.nome} está sem flechas para atacar!`);
+        } else if(this.totalDeFlechas > 0 && !(Math.abs(this.posicao - inimigo.posicao) > 3)){
+            console.log(`${this.nome} não pode atacar ${inimigo.nome} pois estão próximos ${this.posicao} - ${inimigo.posicao}`);
+        }
+    }
 
-console.log(personagem1.atacar(personagem2));
-console.log(personagem2.atacar(personagem1));
-console.log(personagem1.recuperarVida(15, personagem2));
+    recarregarFlecha(quantidade) {
+        this.totalDeFlechas = this.totalDeFlechas + quantidade;
+        return this.totalDeFlechas;
+    }
+}
+
+class Guerreiro extends Personagem {
+    constructor(nome, ataque, defesa, vida, posicao, vivo = true, escudo) {
+        super(nome, ataque, defesa, vida, posicao, vivo)
+        this.escudo = escudo;
+    }
+
+    tomarDano(quantidade) {
+        console.log(`${this.nome} sofre dano de ${quantidade}, mas defendeu com ${this.escudo} de escudo`);
+        if (quantidade > this.escudo) {
+            quantidade = quantidade - this.escudo;
+        } else {
+            quantidade = 0;
+        }
+        super.tomarDano(quantidade);
+    }
+
+    atacar(inimigo) {
+        if(Math.abs(inimigo.posicao - this.posicao) < 2) {
+            super.atacar(inimigo);
+        } else {
+            console.log(`${inimigo.nome} muito distante para ${this.nome} atacar.`)
+        }
+    }
+}
+
+class Mago extends Personagem {
+    constructor(nome, ataque, defesa, vida, posicao, vivo = true) {
+        super(nome, ataque, defesa, vida, posicao, vivo)
+    }
+}
+
+let persoGuerreiro = new Guerreiro("Aragorn", 10, 12, 100, 5, true, 5);
+let persoMago = new Mago("Gendalf", 12, 8, 85, 2);
+let persoArqueiro = new Arqueiro("Legolas", 18, 9, 60, 15, true, 7);
+let persoArqueiro2 = new Arqueiro("Robin Hood", 15, 9, 60, 11, true, 8);
+
+// console.log(persoGuerreiro.atacar(persoMago));
+// console.log(persoMago.atacar(persoGuerreiro));
+
+console.log(persoArqueiro.atacar(persoArqueiro2));
+console.log(persoArqueiro.atacar(persoArqueiro2));
+console.log(persoArqueiro.atacar(persoArqueiro2));
+
+
+console.log(`Total de flechas do ${persoArqueiro.nome} agora é ${persoArqueiro.recarregarFlecha(5)}`);
